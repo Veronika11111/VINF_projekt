47582caefc5c8dfaae2f8d98dec9b9455f738c44
Veronika Magdaléna Rajňáková <veronika.rajnakova@gmail.com>
2020-11-21 10:51:38 +0100

    zoznam susedov je celý v RAMke

--- PageRank.py
+++ PageRank.py
@@ -2,7 +2,7 @@ import json
 import time
 
 start_time = time.time()
-files_suffix = "small"
+files_suffix = "1000000000"
 links_file = open("links_" + files_suffix + ".txt", "r", encoding="utf8")
 iterations_file = open("iterations_" + files_suffix + ".txt", "w", encoding="utf8")
 
@@ -10,18 +10,16 @@ page_ranks = dict()
 default_page_rank = 0.25
 
 # initialization of page_ranks dict
-while(1):
-    line = links_file.readline()
-    if not line:
-        break
-    line = line.strip()
-    line = line.strip(",")
+for line in links_file:
+    line = line.strip(",\n")
     link_object = json.loads(line)
-    page_ranks[link_object.get('l')] = list([default_page_rank, 0])
+    page_ranks[link_object.get('l')] = list([default_page_rank, 0, link_object.get('n')])
     for neighbour in link_object.get('n'):
-        page_ranks[neighbour] = list([default_page_rank, 0])
+        if neighbour not in page_ranks:
+            page_ranks[neighbour] = list([default_page_rank, 0, []])
 
 # now, the dict is initialized to default pageranks for all pages, which were in file links
+print("dictionary inicializovaná")
 
 threshold_value = 0.01
 biggest_change = 100        # only for beginning
@@ -33,30 +31,33 @@ while biggest_change > threshold_value:
 
     biggest_change = 0
     no_iterations = no_iterations+1
-    if no_iterations > 100:
+    if no_iterations > 50:
+        print("koniec, lebo preslo 50 iteracii")
         break
 
-    links_file.seek(0)
+    for link, pr_values in page_ranks.items():
+        number_of_neighbours = len(pr_values[2]) or 1
+        donated_PR = pr_values[0]/number_of_neighbours
+        for neighbour in pr_values[2]:
+            page_ranks[neighbour][1] = page_ranks[neighbour][1] + donated_PR
 
-    while(1):
-        line = links_file.readline()
-        if not line:
-            break
-        line = line.strip()
-        line = line.strip(",")
-        link_object = json.loads(line)
-        number_of_neighbours = len(link_object.get('n'))
-        donated_PR = page_ranks[link_object.get('l')][0]/number_of_neighbours
-        for neighbour in link_object.get('n'):
-           page_ranks[neighbour][1] = page_ranks[neighbour][1] + donated_PR
+    # for line in links_file:
+    #     line = line.strip(",\n")
+    #     link_object = json.loads(line)
+    #     number_of_neighbours = len(link_object.get('n'))
+    #     donated_PR = page_ranks[link_object.get('l')][0]/number_of_neighbours
+    #     for neighbour in link_object.get('n'):
+    #        page_ranks[neighbour][1] = page_ranks[neighbour][1] + donated_PR
 
     for link in page_ranks:
-        if abs(page_ranks[link][0] - page_ranks[link][1]) > biggest_change:
-            biggest_change = abs(page_ranks[link][0] - page_ranks[link][1])
+        change = abs(page_ranks[link][0] - page_ranks[link][1])
+        if change > biggest_change:
+            biggest_change = change
         page_ranks[link][0] = page_ranks[link][1]   # this iteration value to old iteration value
-        page_ranks[link][1] = 0     # new iteration value will be 0
+        page_ranks[link][1] = 0.0    # new iteration value will be 0
 
     iterations_file.write(str(no_iterations) + " -> " + "{:.10f}".format(biggest_change) + "\n")
+    print(str(no_iterations) + " -> " + "{:.10f}".format(biggest_change) + "\n")
 
 iterations_file.write("default page rank= " + "{:.5f}".format(default_page_rank) + "\n")
 iterations_file.write("threshold= " + "{:.5f}".format(threshold_value) + "\n")
@@ -66,8 +67,8 @@ iterations_file.write("--- %s seconds ---" % (time.time() - start_time))
 results_file = open('results_file_' + files_suffix + '.txt', 'w', encoding="utf8")
 
 for link, pr_values in sorted(page_ranks.items(), key=lambda item: item[1][0], reverse=True):
-    results_file.write(link + ":" + "{:.10f}".format(pr_values[0]) + "\n")
-    if pr_values[0] == 0:
-        break
+    results_file.write(link + ":" + "{:.20}".format(pr_values[0]) + "\n")
+    # if pr_values[0] < 0.000000000001:
+    #     break
 
 results_file.close()
